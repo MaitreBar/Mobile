aea61abe43bed758ef1d804055f1dc0b8894263a
BeatrizFrancaGusmaoo <beatriz.gusmao@sptech.school>
2023-09-12 00:53:19 -0300

    Adicionando telas de Categoria, Estabelecimentos mais visitados, Lista de estabelecimentos, Perfil e Alterar perfil

---
+++ app/src/main/java/com/example/maitreapp/AlterarPerfil.kt
@@ -0,0 +1,11 @@
+package com.example.maitreapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AlterarPerfil : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alterar_perfil)
+    }
+}
