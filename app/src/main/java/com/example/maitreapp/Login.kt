1786d3e042630512f8d8c5ffa05c992da1f1358f
landineo <lucas.rodrigues@sptech.school>
2023-10-21 18:09:13 -0300

    feita a integração da home para as telas de login e cadastro

--- app/src/main/java/com/example/maitreapp/Login.kt
+++ app/src/main/java/com/example/maitreapp/Login.kt
@@ -2,10 +2,14 @@ package com.example.maitreapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.maitreapp.databinding.ActivityLoginBinding
 
 class Login : AppCompatActivity() {
+    val binding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/example/maitreapp/Perfil.kt
+++ app/src/main/java/com/example/maitreapp/Perfil.kt
@@ -1,19 +1,11 @@
 package com.example.maitreapp
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 
 class Perfil : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_perfil)
-
-        val testeButton = findViewById<Button>(R.id.teste)
-        testeButton.setOnClickListener{
-            val intent = Intent(applicationContext, MainActivity::class.java)
-            startActivity(intent)
-        }
     }
 }
