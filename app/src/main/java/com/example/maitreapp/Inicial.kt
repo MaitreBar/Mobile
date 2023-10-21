6c36e1cc0f7bd168efa3ae4b36a99597b5ac234d
cesarLimaz29 <cesa.de.sousa.lima@accenture.com>
2023-10-21 20:39:21 -0300

    Subindo tela LGPD e Inicial

---
+++ app/src/main/java/com/example/maitreapp/Inicial.kt
@@ -0,0 +1,11 @@
+package com.example.maitreapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Inicial : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicial)
+    }
+}
