ca51dcf8dc420b6f98cbfea1786d6cd582dbfc71
Mayla Bisacchi <Mayla.Bisacchi@sptech.school>
2023-10-23 11:30:56 -0300

    Criação de FilaEstabelecimento e TelefoneEstabelecimento, assim como adições na strings.xml

---
+++ app/src/main/java/com/example/maitreapp/TelefoneEstabelecimento.kt
@@ -0,0 +1,11 @@
+package com.example.maitreapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TelefoneEstabelecimento : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_telefone_estabelecimento)
+    }
+}
