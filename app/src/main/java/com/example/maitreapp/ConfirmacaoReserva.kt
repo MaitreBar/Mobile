06af18fb95c7a3638eedf07722d63401c3c01427
Mayla Bisacchi <Mayla.Bisacchi@sptech.school>
2023-10-23 10:42:16 -0300

    Criação da tela de ConfirmacaoReserva em  XML, alem de adicoes de imagem e linha no themes e strings.

--- app/src/main/java/com/example/maitreapp/ConfirmacaoReserva.kt
+++ app/src/main/java/com/example/maitreapp/ConfirmacaoReserva.kt
@@ -3,9 +3,9 @@ package com.example.maitreapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class cancelamento_reserva : AppCompatActivity() {
+class ConfirmacaoReserva : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cancelamento_reserva)
+        setContentView(R.layout.activity_confirmacao_reserva)
     }
 }
